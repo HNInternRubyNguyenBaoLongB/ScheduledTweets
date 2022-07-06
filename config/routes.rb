9c8e02fb656ffe058a72c2bd77e5f24e3e633c0d
baolong187 <nguyen.bao.long-b@sun-asterisk.com>
2022-07-06 14:04:32 +0700

    create sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # get 'home/index'
   get "about-us", to: "about#index", as: :about
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
