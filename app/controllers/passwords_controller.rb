18b32fa53c6d8e763ed20934f19f0b15207980c5
baolong187 <nguyen.bao.long-b@sun-asterisk.com>
2022-07-07 09:06:25 +0700

    create password controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -14,6 +14,6 @@ class PasswordsController < ApplicationController
     private 
     def password_params
         params.require(:user).permit(:password, :password_confirmation)
-        
+
     end
 end
